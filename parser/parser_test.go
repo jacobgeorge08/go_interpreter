c4614d7bdec5a391cf726a6eb59b6092f18a72e3
T460 <jacobgeorge08@gmail.com>
2025-05-27 13:09:49 +0530

    added error handling to our parser

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -15,6 +15,8 @@ let foobar = 838383;
 	l := lexer.New(input)
 	p := New(l)
 	program := p.ParseProgram()
+	checkParseErrors(t, p)
+
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
 	}
@@ -35,6 +37,19 @@ let foobar = 838383;
 			return
 		}
 	}
+}
+
+func checkParseErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("Parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
 }
 
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
